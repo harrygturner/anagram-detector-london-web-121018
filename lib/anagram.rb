9f284f1ffccf287b06c024af7915c19671df0ea7
github username <github email address>
2018-12-16 17:57:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  @all = []
+
+  def self.all
+    @all
+  end
+
+  def initialize(word)
+    @word = word
+    self.class.all << self
+  end
+
+  def match(array_of_strings)
+    anagram = []
+    array_of_strings.select do |arr_word|
+      if arr_word.split("").sort == @word.split("").sort
+        anagram << arr_word
+      end
+    end
+    anagram
+  end
+end
+
+#Test Method
+
+# listen = Anagram.new("listen")
+# array_of_strings = %w(enlists gogle inlets banana)
+#
+# binding.pry
